0e3ab9ec92552403c525215ec1728ddd80f05df7
맹주영 <mjy9088@naver.com>
2022-02-22 21:11:35 +0900

    clean temp files

--- build/scripts/clean.sh
+++ build/scripts/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find . -type f -a \( -name "*.o" -o -name "*.d" \) -delete
+find . -type f -a \( -name "*.o" -o -name "*.d" -o -name "*.bc" -o -name "*.i" -o -name "*.s" \) -delete
 find . -type d -empty -delete
