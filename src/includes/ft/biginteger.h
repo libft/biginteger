156ed353d86ece408c3c9b1d6c1750582cf22947
Juyeong Maing <mjy9088@naver.com>
2022-03-21 09:41:19 +0000

    add more prototypes

--- src/includes/ft/biginteger.h
+++ src/includes/ft/biginteger.h
@@ -14,14 +14,13 @@
 # define BIGINTEGER_H
 
 # include <stddef.h>
-# include <stdbool.h>
 # include <stdint.h>
 
 typedef struct s_biginteger	t_biginteger;
 
-t_biginteger				*g_biginteger_one;
-t_biginteger				*g_biginteger_zero;
-t_biginteger				*g_biginteger_ten;
+const t_biginteger			*g_biginteger_one;
+const t_biginteger			*g_biginteger_zero;
+const t_biginteger			*g_biginteger_ten;
 
 t_biginteger	*new_biginteger(int32_t value);
 
@@ -32,6 +31,32 @@ t_biginteger	*new_biginteger_div(t_biginteger *lhs, t_biginteger *rhs);
 t_biginteger	*new_biginteger_mod(t_biginteger *lhs, t_biginteger *rhs);
 t_biginteger	*new_biginteger_sum(t_biginteger **array, size_t length);
 t_biginteger	*new_biginteger_product(t_biginteger **array, size_t length);
+
+bool			biginteger_add(
+					t_biginteger *result,
+					t_biginteger *lhs,
+					t_biginteger *rhs);
+bool			biginteger_sub(
+					t_biginteger *result,
+					t_biginteger *lhs,
+					t_biginteger *rhs);
+bool			biginteger_mul(
+					t_biginteger *result,
+					t_biginteger *lhs,
+					t_biginteger *rhs);
+bool			biginteger_div(
+					t_biginteger *result_div,
+					t_biginteger *result_mod,
+					t_biginteger *lhs,
+					t_biginteger *rhs);
+bool			biginteger_sum(
+					t_biginteger *result,
+					t_biginteger **arr,
+					size_t length);
+bool			biginteger_product(
+					t_biginteger *result,
+					t_biginteger **arr,
+					size_t length);
 
 t_biginteger	*delete_biginteger(t_biginteger *self);
 
